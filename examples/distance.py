5e31222abb4d650de372b9bbe09586ec5e4b1cdb
Radek Brich <radek.brich@devl.cz>
2017-01-28 15:16:57 +0100

    distance demo: added controls to change curve level

--- examples/distance.py
+++ examples/distance.py
@@ -8,31 +8,79 @@ import numpy.linalg as la
 WIDTH = 500
 HEIGHT = 500
 
+LEVEL_NAME = {
+    2: "line",
+    3: "quadratic bézier",
+    4: "cubic bézier",
+}
+
+
+def quadratic_bezier(t, p0, p1, p2):
+    tc = 1 - t
+    p0, p1, p2 = map(np.array, [p0, p1, p2])
+    return tc*tc * p0 + 2*tc*t * p1 + t*t * p2
+
+
+def cubic_bezier(t, p0, p1, p2, p3):
+    tc = 1 - t
+    p0, p1, p2, p3 = map(np.array, [p0, p1, p2, p3])
+    return tc*tc*tc * p0 + 3*tc*tc*t * p1 + 3*tc*t*t * p2 + t*t*t * p3
+
+
+def line_distance(p, p0, p1):
+    p0, p1, p = map(np.array, [p0, p1, p])
+    a = p - p0
+    b = p1 - p0
+    t = a.dot(b) / b.dot(b)
+    t = min(max(t, 0), 1)
+    x = p0 + t * b
+    # http://stackoverflow.com/a/1560510/6013024
+    side = la.det([b, a])
+    dist = math.copysign(la.norm(x - p), side)
+    return dist, x
+
+
+def quadratic_distance(p, p0, p1, p2):
+    # TODO
+    return 0, [0,0]
+
+
+def cubic_distance(p, p0, p1, p2, p3):
+    # TODO
+    return 0, [0,0]
+
 
 class App:
 
     def __init__(self):
-        self.canvas = tk.Canvas(width=WIDTH, height=HEIGHT, bg="gray")
+        root = tk.Tk()
+        self.label_help = tk.Label(root, text="[+/-] change curve level  [LMB] move point  [RMB] move view", justify=tk.LEFT)
+        self.label_help.pack()
+        self.canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg="gray")
         self.canvas.pack(fill=tk.BOTH, expand=True)
-        self.label = tk.Label()
-        self.label.pack()
+        self.label_dist = tk.Label(root)
+        self.label_dist.pack()
         self.canvas.bind("<ButtonPress-1>", self.on_left_press)
         self.canvas.bind("<ButtonRelease-1>", self.on_left_release)
         self.canvas.bind("<ButtonPress-3>", self.on_right_press)
         self.canvas.bind("<ButtonRelease-3>", self.on_right_release)
         self.canvas.bind("<Motion>", self.on_move)
-        self.canvas.master.wm_title(string="Segment distance")
+        root.bind("<KeyPress-KP_Add>", self.on_key_plus)
+        root.bind("<KeyPress-plus>", self.on_key_plus)
+        root.bind("<KeyPress-KP_Subtract>", self.on_key_minus)
+        root.bind("<KeyPress-minus>", self.on_key_minus)
+        root.wm_title(string="Segment distance")
 
-        points = [(WIDTH * 0.3, HEIGHT * 0.3), (WIDTH * 0.7, HEIGHT * 0.7)]
-        point_x = (WIDTH * 0.6, HEIGHT * 0.2)
-        self.points = [self.create_point(p) for p in points]
-        self.point_x = self.create_point(point_x, "green")
-        self.segment = None
-        self.line_x = None
+        self.level = 2
+        self.points = [(WIDTH * 0.6, HEIGHT * 0.2),  # X point
+                       (WIDTH * 0.2, HEIGHT * 0.4),  # P0
+                       (WIDTH * 0.5, HEIGHT * 0.8),  # P1
+                       (WIDTH * 0.8, HEIGHT * 0.4),  # P2
+                       (WIDTH * 0.8, HEIGHT * 0.8)]  # P3
+        self.recreate_points()
         self.refresh()
 
         self.moving = None
-        self.movable = self.points + [self.point_x]
         self.scrolling = False
 
     def main(self):
@@ -40,10 +88,10 @@ class App:
 
     def on_left_press(self, event):
         mx, my = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
-        tolerance = 6
+        tolerance = 10
         items = self.canvas.find_closest(mx, my)
         for item in items:
-            if item in self.movable:
+            if "point" in self.canvas.gettags(item):
                 x1, y1, x2, y2 = self.canvas.coords(item)
                 if (x1 - tolerance <= mx <= x2 + tolerance
                 and y1 - tolerance <= my <= y2 + tolerance):
@@ -63,51 +111,59 @@ class App:
     def on_move(self, event):
         if self.moving is not None:
             item = self.moving
-            x, y = self.point_coords(item)
+            idx = self.canvas.find_withtag("point").index(item)
+            x, y = self.points[idx]
             rx = self.canvas.canvasx(event.x) - x
             ry = self.canvas.canvasy(event.y) - y
             self.canvas.move(item, rx, ry)
+            self.points[idx] = (x + rx, y + ry)
             self.refresh()
         if self.scrolling:
             self.canvas.scan_dragto(event.x, event.y, 1)
 
-    def point_coords(self, item):
-        x1, y1, x2, y2 = self.canvas.coords(item)
-        x = x1 + (x2 - x1) / 2
-        y = y1 + (y2 - y1) / 2
-        return x, y
+    def on_key_plus(self, event):
+        if self.level < 4:
+            self.level += 1
+            self.recreate_points()
+            self.refresh()
+
+    def on_key_minus(self, event):
+        if self.level > 2:
+            self.level -= 1
+            self.recreate_points()
+            self.refresh()
+
+    def recreate_points(self):
+        self.canvas.delete("point")
+        color = "red"  # color for X point
+        radius = 3
+        for p in self.points[:self.level+1]:
+            coords = (p[0] - radius, p[1] - radius, p[0] + radius, p[1] + radius)
+            self.canvas.create_oval(coords, fill=color, outline="white", tag="point")
+            color = "green"  # color for control points
 
     def refresh(self):
-        if self.segment is not None:
-            self.canvas.delete(self.segment)
-        points = [self.point_coords(item) for item in self.points]
-        self.segment = self.canvas.create_line(points, width=2, fill="white")
+        self.canvas.delete("line")
+        points = self.points[1:self.level+1]
+        if self.level == 2:
+            self.canvas.create_line(points, width=2, fill="white", tag="line")
+        else:
+            func = {3: quadratic_bezier, 4: cubic_bezier}[self.level]
+            smoothness = 100
+            start = points[0]
+            for t in range(1, smoothness + 1):
+                imp = func(t / smoothness, *points)
+                self.canvas.create_line(list(start), list(imp), width=2, fill="white", tag="line")
+                start = imp
 
-        px = self.point_coords(self.point_x)
-        dist, x = self.signed_distance(px)
-        self.label.config(text="distance: %03f" % dist)
-        if self.line_x is not None:
-            self.canvas.delete(self.line_x)
-        self.line_x = self.canvas.create_line(list(x), px, width=2, fill="yellow", dash=(3,5))
-        self.canvas.tag_raise("point", self.line_x)
+        px = self.points[0]
+        dist_func = {2: line_distance, 3: quadratic_distance, 4: cubic_distance}[self.level]
+        dist, x = dist_func(px, *self.points[1:self.level+1])
+        self.label_dist.config(text="%s distance: %.2f" % (LEVEL_NAME[self.level], dist))
+        self.canvas.create_line(list(x), px, width=2, fill="yellow", dash=(3,5), tag="line")
 
-    def create_point(self, p, color="red"):
-        radius = 3
-        coords = (p[0] - radius, p[1] - radius, p[0] + radius, p[1] + radius)
-        return self.canvas.create_oval(coords, fill=color, outline="white", tag="point")
-
-    def signed_distance(self, p):
-        p0, p1 = [self.point_coords(item) for item in self.points]
-        p0, p1, p = map(np.array, [p0, p1, p])
-        a = p - p0
-        b = p1 - p0
-        t = a.dot(b) / b.dot(b)
-        t = min(max(t, 0), 1)
-        x = p0 + t * b
-        # http://stackoverflow.com/a/1560510/6013024
-        side = la.det([b, a])
-        dist = math.copysign(la.norm(x - p), side)
-        return dist, x
+        # Bring points to front (above lines)
+        self.canvas.tag_raise("point", "line")
 
 
 App().main()
